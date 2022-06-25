b48e1343c5aef1f966a50c22e8153782c0b5e113
shanmugam1099 <100747555+shanmugam1099@users.noreply.github.com>
2022-06-25 23:12:08 +0530

    Update deploy.groovy

--- deploy.groovy
+++ deploy.groovy
@@ -59,7 +59,7 @@ def runPipeline(props){// Deployment start
                 sh """#!/bin/bash +e
                 cd Deployment
                 ls
-                kubectl get pod -n kube-system
+                kubectl get pod -n kube-system --kubeconfig=config
                 echo " DOCKER_TAG=\$(cat changeover.yaml | shyaml get-value baseImageName.$SelectList)"
                 echo "\$(cat changeover.yaml | shyaml get-value baseImageName.$SelectList)" > IMG.txt
                 TAG=\$(cat IMG.txt)
