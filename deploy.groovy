3544e7aff110892738d13189d1cf7b0cfc2e3917
shanmugam1099 <100747555+shanmugam1099@users.noreply.github.com>
2022-08-18 13:19:45 +0530

    Update deploy.groovy

--- deploy.groovy
+++ deploy.groovy
@@ -53,7 +53,7 @@ def runPipeline(props){// Deployment start
  def runDevDeployStages(SelectList){
 
             echo "Deploying this service ${SelectList} ............. "
-             lisTUser=(props.ldapApprovalGroup.contains)
+             lisTUser = props.ldapApprovalGroup
              echo "lisTUser $lisTUser"
           //  echo "kubectl deploy ${SelectList}"
           //  echo " If required pip install shyaml"
