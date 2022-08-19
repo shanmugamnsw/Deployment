21eff712434b62f957061922d95f00ae8748dbf2
shanmugam1099 <100747555+shanmugam1099@users.noreply.github.com>
2022-08-19 16:22:54 +0530

    Update deploy.groovy

--- deploy.groovy
+++ deploy.groovy
@@ -35,7 +35,7 @@ def runPipeline(props){// Deployment start
        }
 
 //        if (!isStartedByTimer())
-      if ((env.inputEnvType != 'PROD') && (env.inputEnvType != 'STG') && (allow == "false")){
+      if ((env.inputEnvType != 'PROD') && (env.inputEnvType != 'STG') && (allow != "false")){
             error "You are not allowed to run deployment in Non-DEV environments."
             }
           //  }
