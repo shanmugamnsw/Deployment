052541557c96567527d459cdd1e14f02d746d732
shanmugam1099 <100747555+shanmugam1099@users.noreply.github.com>
2022-08-19 10:23:19 +0530

    Update deploy.groovy

--- deploy.groovy
+++ deploy.groovy
@@ -32,7 +32,7 @@ def runPipeline(props){// Deployment start
        }
 
 //        if (!isStartedByTimer()){
-      if ((env.inputEnvType != 'PROD') && (env.inputEnvType != 'STG') && (!props.ldapApprovalGroup.contains(adm))){
+      if ((env.inputEnvType != 'PROD') && (env.inputEnvType != 'STG') && (!props.ldapApprovalGroup.contains(${currentBuild.getBuildCauses()[0].userId}))){
             error "You are not allowed to run deployment in Non-DEV environments."
             }
           //  }
